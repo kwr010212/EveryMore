7737540dfb0f2ae6718a4e6e5dc5e16a577397cd
kwr010212 <k67891375@gmail.com>
2026-04-21 15:24:00 +0900

    step 3.질문 출력과 기본 동작과 UI

--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,23 @@
+import random
 from django.shortcuts import render
-from .models import Question
+from .models import Question, Category
 
 def home(request):
-    questions = Question.objects.all()
-    return render(request, 'home.html', {'questions': questions})
+    categories = Category.objects.all()
+    selected_category = request.GET.get('category')
+
+    if selected_category:
+        questions = Question.objects.filter(category_id=selected_category)
+    else:
+        questions = Question.objects.all()
+
+    if questions:
+        question = random.choice(questions)
+    else:
+        question = None
+
+    return render(request, 'home.html', {
+        'question': question,
+        'categories': categories,
+        'selected_category': selected_category
+    })
